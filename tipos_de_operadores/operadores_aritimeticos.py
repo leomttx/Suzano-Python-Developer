a46b6bf9d444290ea8254c5966533531be7a7287
Leonardo Teixeira Martins <leonardo.t@escolar.ifrn.edu.br>
2025-04-09 00:11:09 -0300

    nc: Conteúdo de operadores aritimeticos

---
+++ tipos_de_operadores/operadores_aritimeticos.py
@@ -0,0 +1,19 @@
+produto_1 = 5
+produto_2 = 10
+
+print(produto_1 + produto_2) # soma
+print(produto_1 - produto_2) # subtração
+print(produto_1 * produto_2) # multiplicação
+print(produto_1 / produto_2) # divisão
+print(produto_1 // produto_2) # divisão inteira
+print(produto_1 % produto_2) # resto da divisão(módulo)
+print(produto_1 ** produto_2) # potência
+
+expressao = 5 * 2 + 3 - 1 / 4 ** 2 
+print(expressao) 
+
+# Precedência de operadores
+# 1. Parênteses () 
+# 2. Potência **
+# 3. Multiplicação e divisão * / // %   
+# 4. Adição e subtração + -
