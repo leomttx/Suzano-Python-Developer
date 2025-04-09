7f8bf92f12f03635439c70c2054c144972a9dcb6
Leonardo Teixeira Martins <leonardo.t@escolar.ifrn.edu.br>
2025-04-08 23:15:48 -0300

    incio: aprendendo o básico

---
+++ basico/convertendo_tipos.py
@@ -0,0 +1,21 @@
+print(int(10.5)) # converte float para int, truncando a parte decimal
+print(float(10)) # converte int para float
+print(str(10)) # converte int para str
+
+print(bool(0)) # converte int para bool, 0 é considerado False
+print(bool(1)) # converte int para bool, 1 é considerado True
+
+print(bool("")) # converte str para bool, string vazia é considerada False
+print(bool(" ")) # converte str para bool, string com espaço é considerada True
+
+valor = 10.5
+valor_str = str(valor) # converte float para str
+
+print(type(valor)) # imprime o tipo da variável valor
+print(type(valor_str)) # imprime o tipo da variável valor_str
+
+
+print(10 / 5) # divisão normal, resultado é float
+print(10 // 5) # divisão inteira, resultado é int
+print(10 % 3) # resto da divisão, resultado é int
+
---
+++ basico/modo_interativo.py
@@ -0,0 +1,9 @@
+# Ao digitar "python3" no terminal, o interpretador Python é ativado em modo interativo.
+# O prompt ">>>" indica que o interpretador está pronto para receber comandos.
+# Para sair do modo interativo, digite "exit()" ou "Ctrl+D".
+
+# O comando dir() exibe uma lista de todos os nomes definidos no escopo atual.
+# Nessa lista, estão incluídos os nomes de variáveis, módulos, funções, etc.
+
+# O comando help() exibe uma mensagem de ajuda interativa.
+# Essas mensagens de ajuda são úteis para entender como usar um objeto ou função.
