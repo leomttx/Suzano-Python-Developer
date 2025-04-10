10a6f07d0b2733e2418d1844e5a12114b4b6b9d7
Leonardo Teixeira Martins <leonardo.t@escolar.ifrn.edu.br>
2025-04-09 22:03:56 -0300

    nc: Conteúdo de operadores de comparação

---
+++ operadores_de_comparacao/operadores_de_comparação.py
@@ -0,0 +1,11 @@
+saldo = 400
+saque = 500
+
+print(saldo > saque)  # False
+print(saldo < saque)  # True
+print(saldo >= saque)  # False
+print(saldo <= saque)  # True   
+print(saldo == saque)  # False
+print(saldo != saque)  # True   
+print(saldo == 400)  # True
+print(saldo != 400)  # False
