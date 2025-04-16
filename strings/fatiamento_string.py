1e423c5a2063c0bcc871ab86ab6fa0252aa0bfd2
Leonardo Teixeira Martins <leonardo.t@escolar.ifrn.edu.br>
2025-04-16 00:25:39 -0300

    nc: técnica de fatiamento de string

---
+++ strings/fatiamento_string.py
@@ -0,0 +1,9 @@
+nome = "Leonardo"
+
+print(nome[0])
+print(nome[-1])
+print(nome[0:]) #start
+print(nome[:9]) #stop
+print(nome[0:3]) #start e stop
+print(nome[0:9:2]) #step(intervalo)
+print(nome[::-1]) #espelhamento
